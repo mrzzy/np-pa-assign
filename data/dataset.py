caa997827637fd79a6e3123386eb7ac6dd2dab7c
Zhu Zhanyan <program.nom@gmail.com>
2020-01-25 08:53:55 +0800

    Initial Commit: Added EDA and dataprep notebooks

---
+++ data/dataset.py
@@ -0,0 +1,48 @@
+#
+# NP PA Assignment
+# Dataset Metadata
+#
+
+
+cat_features = [
+    "Mjob",
+    "Fjob",
+    "guardian",
+    "reason"
+]
+
+bin_features = [
+    "sex",
+    "school",
+    "address",
+    "Pstatus",
+    "famsize",
+    "schoolsup",
+    "famsup",
+    "activities",
+    "paid",
+    "internet",
+    "nursery",
+    "higher",
+    "romantic"
+]
+
+num_features = [
+    "age",
+    "Medu",
+    "Fedu",
+    "famrel",
+    "traveltime",
+    "studytime",
+    "failures",
+    "freetime",
+    "goout",
+    "Walc",
+    "Dalc",
+    "health",
+    "absences",
+    "G1",
+    "G2"
+]
+
+target_var = "G3"
